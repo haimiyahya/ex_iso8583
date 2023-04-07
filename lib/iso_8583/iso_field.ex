98067a8ae9cb078fd1411ac79b8774b3bcfb6112
Mohd Norhaimi <haimiyahya@gmail.com>
2023-04-07 23:36:49 +0800

    remove the wrong test in test/msg_test.exs

--- lib/iso_8583/iso_field.ex
+++ lib/iso_8583/iso_field.ex
@@ -88,7 +88,6 @@ defmodule IsoField do
         |> Util.check_if_required_pad_left(header_size, data_type, max_len)
 
       :binary ->
-
         bin_length = trunc(max_len/8)
         String.slice(field_value, 0, bin_length)
 
